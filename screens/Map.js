e4001e14dd5dee939927ea0af0211e6958ac1628
austinChappell <Austin.W.Chappell@gmail.com>
2018-01-28 22:59:59 -0600

    add mapview libary

--- screens/Map.js
+++ screens/Map.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-elements';
 import NavBar from '../components/NavBar';
+import MapView from 'react-native-maps';
 
-const Map = ({ navigation }) => {
-  return (
-    <View>
-      <NavBar navigation={navigation} />
-      <Text>
-        Map Screen
-      </Text>
-    </View>
-  )
+class Map extends Component {
+
+  render() {
+
+    const { navigation } = this.props;
+
+    return (
+      <View>
+        <NavBar navigation={navigation} />
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+        <Text>
+          Map Screen
+        </Text>
+      </View>
+    )
+  }
 }
 
 export default Map;
