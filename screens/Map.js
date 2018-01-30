4786c04c1d19cbe6a6f7dcb8d7376de4e075adec
austinChappell <Austin.W.Chappell@gmail.com>
2018-01-29 22:09:24 -0600

    add map

--- screens/Map.js
+++ screens/Map.js
@@ -1,32 +1,55 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
-import NavBar from '../components/NavBar';
+import { StyleSheet, View } from 'react-native';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
 import MapView from 'react-native-maps';
+import NavBar from '../components/NavBar';
 
-class Map extends Component {
+Mapbox.setAccessToken('pk.eyJ1IjoiYXVzdGlud2MzMyIsImEiOiJjamN6cHV3Y3cxbmQ5Mnluc2J0MW1ianFrIn0.1VXh5wkY_-sCJLHxrmPomw');
 
+export default class App extends Component<{}> {
   render() {
-
     const { navigation } = this.props;
-
+    const markers = [
+      {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        title: 'Foo Place',
+        subtitle: '1234 Foo Drive'
+      }
+    ];
     return (
-      <View>
+      <View style={styles.container}>
         <NavBar navigation={navigation} />
         <MapView
+          style={styles.container}
+          annotations={markers}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: 32.777,
+            longitude: -96.797,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-        />
-        <Text>
-          Map Screen
-        </Text>
+        >
+          <MapView.Marker
+            coordinate={{latitude: 32.777,
+            longitude: -96.797}}
+            title={"A Gig"}
+            description={"House of Blues"}
+          />
+        </MapView>
+        {/* <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Street}
+            zoomLevel={12}
+            centerCoordinate={[-96.797, 32.777]}
+            style={styles.container}>
+        </Mapbox.MapView> */}
       </View>
-    )
+    );
   }
 }
 
-export default Map;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
