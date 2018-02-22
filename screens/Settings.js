75269284739d9fca283cad96001f7f8fa7fd8988
austinChappell <Austin.W.Chappell@gmail.com>
2018-02-22 06:49:56 -0600

    add instrument and genre pickers

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,57 +1,80 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, List, ListItem } from 'react-native-elements';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
+import { Button, Header, Icon, List, ListItem } from 'react-native-elements';
 
 import GoBackNavBar from '../components/GoBackNavBar';
 import SettingsOptions from './SettingsScreens/';
+import { colors, styles } from '../assets/styles';
 
 class Settings extends Component {
-
   state = {
-    selection: null,
-  }
+    headerTitle: '',
+    selection: null
+  };
 
-  changeSelection = (selection) => {
-    this.setState({selection})
-  }
+  changeSelection = (selection, headerTitle) => {
+    this.setState({ selection, headerTitle });
+  };
 
   render() {
-
     const { navigation } = this.props;
     const { selection } = this.state;
-
-    const mainContent = <View style={{ flex: 1, flexDirection: 'column' }}>
-      <GoBackNavBar navigation={navigation} logoutButton={true} />
-      <View style={{ flexGrow: 1 }}>
-        <List>
-          <ListItem
-            onPress={() => console.log('ACCOUNT SETTINGS CLICKED')}
-            title="Account Settings"
-          />
-          <ListItem
-            onPress={() => this.changeSelection('profile')}
-            title="Profile Settings"
-          />
-          <ListItem
-            onPress={() => console.log('NOTIFICATION SETTINGS CLICKED')}
-            title="Notification Settings"
-          />
-        </List>
-      </View>
-      <View style={{ paddingBottom: 30 }}>
-        <Button
-          backgroundColor="red"
-          title="Deactivate Account"
+    const innerContent = SettingsOptions[selection];
+    const selectedContent = (
+      <ScrollView>
+        <Header
+          statusBarProps={{ barStyle: 'dark-content' }}
+          backgroundColor={colors.bgLight}
+          leftComponent={
+            <TouchableOpacity
+              hitSlop={{ top: 20, bottom: 20, left: 50, right: 50 }}
+              onPress={() => this.changeSelection(null, '')}
+            >
+              <Icon
+                name="ios-arrow-back-outline"
+                type="ionicon"
+                color={colors.primary}
+              />
+            </TouchableOpacity>
+          }
+          centerComponent={{
+            text: this.state.headerTitle,
+            style: { color: colors.primary }
+          }}
         />
-      </View>
-    </View>
+        {innerContent}
+      </ScrollView>
+    );
 
-    const content = selection ? 
-    SettingsOptions[selection] : mainContent;
+    const mainContent = (
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <GoBackNavBar navigation={navigation} logoutButton={true} />
+        <View style={{ flexGrow: 1 }}>
+          <List>
+            <ListItem
+              onPress={() => console.log('ACCOUNT SETTINGS CLICKED')}
+              title="Account Settings"
+            />
+            <ListItem
+              onPress={() => this.changeSelection('profile', 'Profile')}
+              title="Profile Settings"
+            />
+            <ListItem
+              onPress={() => console.log('NOTIFICATION SETTINGS CLICKED')}
+              title="Notification Settings"
+            />
+          </List>
+        </View>
+        <View style={{ paddingBottom: 30 }}>
+          <Button backgroundColor="red" title="Deactivate Account" />
+        </View>
+      </View>
+    );
+
+    const content = selection ? selectedContent : mainContent;
 
     return content;
   }
-
-} 
+}
 
 export default Settings;
--- screens/SettingsScreens/Profile.js
+++ screens/SettingsScreens/Profile.js
@@ -1,20 +1,212 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { Picker, Text, TextInput, View } from 'react-native';
+import { Avatar, Card } from 'react-native-elements';
+import { Form, Item, Input, Label } from 'native-base';
+
+import { colors, styles } from '../../assets/styles';
+
+const genreOptions = [
+  { label: '---', value: '' },
+  { label: 'Jazz', value: 'jazz' },
+  { label: 'Rock', value: 'rock' },
+  { label: 'Country', value: 'country' },
+  { label: 'Blues', value: 'blues' }
+];
+
+const instrumentOptions = [
+  { label: '---', value: '' },
+  { label: 'Drum Set', value: 'drum_set' },
+  { label: 'Bass', value: 'bass' },
+  { label: 'Elec. Gtr.', value: 'elec_gtr' },
+  { label: 'Piano', value: 'piano' }
+];
 
 class Profile extends Component {
+  state = {
+    email: 'austin@test.com',
+    firstName: 'Austin',
+    lastName: 'Chappell',
+    genres: ['', '', ''],
+    instruments: ['', '', '']
+  };
+
+  handleInputChange = (val, key) => {
+    const o = {};
+    o[key] = val;
+    this.setState(o);
+  };
+
+  handlePickerChange = (val, index, key, stateItemIndex) => {
+    const options = this.state[key];
+    options[stateItemIndex] = val;
+    const o = {};
+    o[key] = options;
+    this.setState(o);
+  };
+
+  selectGenre = (val, index, genreIndex) => {
+    const { genres } = this.state;
+    genres[genreIndex] = val;
+    this.setState({ genres });
+  };
 
   render() {
-
+    console.log('PROFILE STATE', this.state);
     return (
+      <View contentContainerStyle={{ flex: 0 }}>
+        <View style={{ alignItems: 'center', marginTop: 20 }}>
+          <Avatar
+            large
+            rounded
+            source={{
+              uri:
+                'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+            }}
+            onPress={() => console.log('Works!')}
+            activeOpacity={0.7}
+          />
+        </View>
 
-      <View>
-        <Text>Profile Screen</Text>
+        <View>
+          <Card title="Basic Info">
+            <Form>
+              <Item floatingLabel>
+                <Label>First Name</Label>
+                <Input
+                  onChangeText={text =>
+                    this.handleInputChange(text, 'firstName')
+                  }
+                  value={this.state.firstName}
+                />
+              </Item>
+
+              <Item floatingLabel>
+                <Label>Last Name</Label>
+                <Input
+                  onChangeText={text =>
+                    this.handleInputChange(text, 'lastName')
+                  }
+                  value={this.state.lastName}
+                />
+              </Item>
+
+              <Item floatingLabel last>
+                <Label>Notification Email</Label>
+                <Input
+                  onChangeText={text => this.handleInputChange(text, 'email')}
+                  value={this.state.email}
+                />
+              </Item>
+
+              <Item floatingLabel last>
+                <Label>Login Email</Label>
+                <Input
+                  style={{ color: colors.disabled }}
+                  disabled
+                  value={this.state.email}
+                />
+              </Item>
+            </Form>
+          </Card>
+        </View>
+
+        <View>
+          <Card title="Genres of Interest">
+            <Form>
+              {this.state.genres.map((stateGenre, sgIndex) => {
+                return (
+                  <View key={sgIndex}>
+                    <Label>Genre #{sgIndex + 1}</Label>
+                    <Picker
+                      selectedValue={this.state.genres[sgIndex]}
+                      onValueChange={(val, index) =>
+                        this.selectGenre(val, index, sgIndex)
+                      }
+                    >
+                      {genreOptions.map((genre, index) => {
+                        const selectionIndex = this.state.genres.findIndex(
+                          option => option === genre.value
+                        );
+                        if (
+                          selectionIndex < 0 ||
+                          selectionIndex === sgIndex ||
+                          index === 0
+                        ) {
+                          return (
+                            <Picker.Item
+                              key={index}
+                              label={genre.label}
+                              value={genre.value}
+                            />
+                          );
+                        }
+                      })}
+                    </Picker>
+                  </View>
+                );
+              })}
+            </Form>
+          </Card>
+        </View>
+
+        <View style={{ marginBottom: 20 }}>
+          <Card title="Instrument">
+            <Form>
+              {this.state.instruments.map((stateInstrument, instIndex) => {
+                return (
+                  <View key={instIndex}>
+                    <Label>Instrument #{instIndex + 1}</Label>
+                    <Picker
+                      selectedValue={stateInstrument}
+                      onValueChange={(val, index) =>
+                        this.handlePickerChange(
+                          val,
+                          index,
+                          'instruments',
+                          instIndex
+                        )
+                      }
+                    >
+                      {instrumentOptions.map((instrument, index) => {
+                        const selectionIndex = this.state.instruments.findIndex(
+                          option => option === instrument.value
+                        );
+                        console.log(
+                          'SELECTION INDEX',
+                          instrument,
+                          selectionIndex
+                        );
+                        if (
+                          selectionIndex < 0 ||
+                          selectionIndex === instIndex ||
+                          index === 0
+                        ) {
+                          return (
+                            <Picker.Item
+                              key={index}
+                              label={instrument.label}
+                              value={instrument.value}
+                            />
+                          );
+                        }
+                      })}
+                    </Picker>
+                  </View>
+                );
+              })}
+            </Form>
+          </Card>
+        </View>
       </View>
-
-    )
-
+    );
   }
-
 }
 
-export default Profile;
+const mapStateToProps = state => {
+  return {
+    user: state.user.user
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
