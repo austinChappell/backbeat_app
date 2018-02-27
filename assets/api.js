d235ff7e3345c3df942458950882a7687c724075
austinChappell <Austin.W.Chappell@gmail.com>
2018-02-27 07:03:55 -0600

    center map on user zipcode

--- assets/api.js
+++ assets/api.js
@@ -1,114 +1,112 @@
 import { AsyncStorage } from 'react-native';
 import data from './data';
+
 const api = data.apiURL;
 
 class Api {
-
   createUser = (user, cb) => {
-
     console.log('CREATING USER', user, api, cb);
 
     return fetch(`${api}/signup`, {
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       method: 'POST',
-      body: JSON.stringify(user)
-    }).then((response) => {
-      console.log('RESPONSE OF CREATE USER FUNC', response)
-      return response.json()
-    }).then((results) => {
-      console.log('RESULTS FROM CREATE USER FUNC', results)
-      cb(results)
-    }).catch(err => console.error('PROBLEM LOGGING OUT', err))
-
-  }
-
-  getProfile = (username, token, cb) => {
-    return fetch(`${api}/api/profile/${username}/`, {
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        token
-      }
-    }).then((response) => {
-      return response.json()
-    }).then((results) => {
-      console.log('RESULTS', results)
-      cb(results)
+      body: JSON.stringify(user),
     })
-  }
+      .then((response) => {
+        console.log('RESPONSE OF CREATE USER FUNC', response);
+        return response.json();
+      })
+      .then((results) => {
+        console.log('RESULTS FROM CREATE USER FUNC', results);
+        cb(results);
+      })
+      .catch(err => console.error('PROBLEM LOGGING OUT', err));
+  };
 
-  getUserInfo = (userid, token, cb, cbError) => {
-    return fetch(`${api}/api/user/id/${userid}`, {
+  getProfile = (username, token, cb) =>
+    fetch(`${api}/api/profile/${username}/`, {
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    }).then((response) => {
-      return response.json();
-    }).then((results) => {
-      console.log('USER INFO', results);
-      const user = results[0]
-      const {
-        activation_key,
-        first_name,
-        id,
-        is_active,
-        last_name,
-        onboarding_stage,
-       } = user;
-      // cb(user)
-      AsyncStorage.setItem('firstName', first_name)
-      .then(() => {
-        AsyncStorage.setItem('id', String(id))
+        token,
+      },
+    })
+      .then(response => response.json())
+      .then((results) => {
+        console.log('RESULTS', results);
+        cb(results);
+      });
+
+  getUserInfo = (userid, token, cb, cbError) =>
+    fetch(`${api}/api/user/id/${userid}`, {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then((results) => {
+        console.log('USER INFO', results);
+        const user = results[0];
+        const {
+          first_name, id, is_active, last_name, onboarding_stage,
+        } = user;
+        // cb(user)
+        AsyncStorage.setItem('firstName', first_name)
+          .then(() => {
+            AsyncStorage.setItem('id', String(id));
+          })
+          .then(() => {
+            AsyncStorage.setItem('lastName', last_name);
+          })
+          .then(() => {
+            AsyncStorage.setItem('onboardingStage', String(onboarding_stage));
+          })
+          .then(() => {
+            cb(user);
+          });
       })
-      .then(() => {
-        AsyncStorage.setItem('lastName', last_name)
-      })
-      .then(() => {
-        AsyncStorage.setItem('onboardingStage', String(onboarding_stage))
-      })
-      .then(() => {
-        cb(user)
-      })
-    }).catch(err => cbError())
-  }
+      .catch(err => cbError());
 
   login = (credentials, cb) => {
-    console.log('HITTING THE LOGIN ROUTE', credentials, cb)
+    console.log('HITTING THE LOGIN ROUTE', credentials, cb);
     return fetch(`${api}/login`, {
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       method: 'POST',
-      body: JSON.stringify(credentials)
-    }).then((response) => {
-      console.log('RESPONSE FROM LOGIN FUNC', response)
-      return response.json()
-    }).then((results) => {
-      console.log('LOGIN RESULTS', results)
-      cb(results)
-    }).catch((err) => {
-      console.error('LOGIN ERROR', err)
+      body: JSON.stringify(credentials),
     })
-  }
+      .then((response) => {
+        console.log('RESPONSE FROM LOGIN FUNC', response);
+        return response.json();
+      })
+      .then((results) => {
+        console.log('LOGIN RESULTS', results);
+        cb(results);
+      })
+      .catch((err) => {
+        console.error('LOGIN ERROR', err);
+      });
+  };
 
   logout = (cb) => {
     console.log('hitting the logout route');
     return fetch(`${api}/logout`, {
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      method: 'POST'
-    }).then((response) => {
-      return response.json()
-    }).then((results) => {
-      cb(results)
-    }).catch(err => console.error('PROBLEM LOGGING OUT', err))
-  }
-
+      method: 'POST',
+    })
+      .then(response => response.json())
+      .then((results) => {
+        cb(results);
+      })
+      .catch(err => console.error('PROBLEM LOGGING OUT', err));
+  };
 }
 
 export default Api;
--- screens/Map.js
+++ screens/Map.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 import NavBar from '../components/NavBar';
 
-export default class App extends Component<{}> {
-  render() {
-    const { navigation } = this.props;
-    const markers = [
-      {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        title: 'Foo Place',
-        subtitle: '1234 Foo Drive'
-      }
-    ];
-    return (
-      <View style={styles.container}>
-        <NavBar navigation={navigation} />
-        <MapView
-          style={styles.container}
-          annotations={markers}
-          initialRegion={{
+function Map(props) {
+  console.log('USER', props.user);
+  const { navigation, user } = props;
+  const { latitude, longitude } = user;
+  const markers = [
+    {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      title: 'Foo Place',
+      subtitle: '1234 Foo Drive',
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <NavBar navigation={navigation} />
+      <MapView
+        showsUserLocation
+        followsUserLocation
+        style={styles.container}
+        annotations={markers}
+        initialRegion={{
+          latitude: 32.777,
+          longitude: -96.797,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        region={{
+          latitude: latitude || 32.777,
+          longitude: longitude || -96.797,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <MapView.Marker
+          coordinate={{
             latitude: 32.777,
             longitude: -96.797,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
           }}
-        >
-          <MapView.Marker
-            coordinate={{latitude: 32.777,
-            longitude: -96.797}}
-            title={"A Gig"}
-            description={"House of Blues"}
-          />
-        </MapView>
-      </View>
-    );
-  }
+          title="A Gig"
+          description="House of Blues"
+        />
+      </MapView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -44,3 +55,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+const mapStateToProps = state => ({
+  user: state.userReducer.user,
+});
+
+export default connect(mapStateToProps)(Map);
